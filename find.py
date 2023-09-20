292b80c72bff0a3f5068d4be3094f56f0d6fa9cc
zhouqingshan <zhouqingshan216@163.com>
2023-09-20 11:24:41 +0800

    脚本工具测试

--- find.py
+++ find.py
@@ -1,5 +1,7 @@
 import os
 import time
+from openpyxl import Workbook, load_workbook
+
 keyList=[]
 keyWithTime={}
 
@@ -13,6 +15,34 @@ def key_read(filePath):
         line=file.readline()
         i=i+1
     file.close()
+#文件保存
+def save_file():
+    newWb = Workbook()
+    sheet = newWb.active
+    number = 0
+    rows = 1
+    for key in keyWithTime:
+        listnumber = len(keyWithTime[key])
+        temp = number+listnumber
+        #print(f'key:{key} and numer:{listnumber}')
+        number = temp
+        if listnumber > 1:
+            for i in range(listnumber):
+                cell_1 = sheet.cell(row=rows, column=1)
+                cell_1.value = key               
+                cell_2 = sheet.cell(row=rows, column=2)
+                cell_2.value = keyWithTime[key][i]
+                rows+= 1
+        else:
+            cell_1 = sheet.cell(row=rows, column=1)
+            cell_1.value = key
+            cell_2 = sheet.cell(row=rows, column=2)
+            cell_2.value = keyWithTime[key][0]
+            rows+= 1
+        
+        
+    #print(f'number:{number}')
+    newWb.save('result.xlsx')
 #文件内容对比工作
 def work(filePath='log.txt'):
     time=[]
@@ -30,9 +60,11 @@ def work(filePath='log.txt'):
                 keyWithTime[key].append(format(float(time1[2]), ".3f"))
         line=file.readline()
         i=i+1
-    file.close()   
+    file.close() 
+    
 key_read('key.txt') 
 work()
+save_file()
 print(f'keyList: {keyList}')
 print(f'keyWithTime: {keyWithTime}')
 os.system('pause')
